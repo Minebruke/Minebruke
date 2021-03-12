cfa7e3210bde898d074f27e65f34a33bf30f4854
Minebruke <80499695+Minebruke@users.noreply.github.com>
2021-03-12 10:11:38 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  <head>
   <title>Edu Shakirov</title>
  </head>
- <body>
+ <body style="background-color:powderblue;">
   <h1>The best student</h1>
   <p>Group:SCA-18.</p>
   <p>Age:19.</p>
@@ -11,6 +11,6 @@
   <p>City:Bishkek.</p>
   <p>Country: Kyrgyzstan.</p>
   <p>Job: Secret Agent.</p>
-  <p>.</p>
+  <img src = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwhatsism.com%2Fuploads%2Fposts%2F2018-07%2F1530546770_rmk_vdjbx10.jpg&imgrefurl=https%3A%2F%2Fwhatsism.com%2F9892-avatarka-dlya-vatsap-gorilla.html&tbnid=yoyhQNQRJhGYzM&vet=12ahUKEwiBpeiO8anvAhVLCHcKHXdxBkwQMygFegUIARDbAQ..i&docid=uBsx7wJdp7ctHM&w=532&h=600&q=avatarka&client=ubuntu&ved=2ahUKEwiBpeiO8anvAhVLCHcKHXdxBkwQMygFegUIARDbAQ">
  </body>
 </html>
