940f00650fd70d5194a022258a7ce99f11d44028
Minebruke <80499695+Minebruke@users.noreply.github.com>
2021-03-12 09:42:10 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <title>Edu Shakirov</title>
+ </head>
+ <body>
+  <h1>The best student</h1>
+  <!-- My opinion -->
+  <p>First something.</p>
+  <p>Second something.</p>
+ </body>
+</html>
