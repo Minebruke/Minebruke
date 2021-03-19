b01b89efbc65b9ffb4b400e6aed04cd49cb89fb0
Minebruke <80499695+Minebruke@users.noreply.github.com>
2021-03-19 09:18:22 +0600

    Update index.php

--- index.php
+++ index.php
@@ -12,20 +12,5 @@
   <p>Country: Kyrgyzstan.</p>
   <p>Job: Secret Agent.</p>
   <img src = "image.jpeg">
-  <h1>My registration</h1>
- <form action="action_page.php">
-  <div class="container">
-    <h1>Register</h1>
-
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" id="email" required>
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
-
-    <button type="submit" value="Submit">Submit</button> 
-    <button type="reset" value="Reset">Clear</button>
-  </div>
-</form> 
  </body>
 </html>
