9c53a98fa3fc4807125cbfeb4d655adbf3beaece
Minebruke <80499695+Minebruke@users.noreply.github.com>
2021-03-19 09:27:31 +0600

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <title>Edu Shakirov</title>
+ </head>
+ <body style="background-color:powderblue;">
+  <h1>The best student</h1>
+ <form action="action_page.php">
+  <div class="container">
+    <h1>Register</h1>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" id="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+
+    <button type="submit" value="Submit">Submit</button> 
+    </br>
+    <button type="reset" value="Reset">Clear</button>
+  </div>
+</form> 
+ </body>
+</html>
