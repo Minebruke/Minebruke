c64de2e8870c36a0440e9808a93bfcee1b60d8c1
Minebruke <80499695+Minebruke@users.noreply.github.com>
2021-03-19 10:28:47 +0600

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,13 @@
  <form action="main.php">
   <div class="container">
     <h1>Register</h1>
-    <label for="email"><b>Email</b></label>
+    <label for="email"><b>login</b></label>
     <input type="text" placeholder="Enter Email" name="email" id="email" required>
     <label for="psw"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+    if (login == 'Eldiyar' && Password == '1234'
+       alert ('Welcome ,pls wait')
+    else alert('Incorrect login or password')
     <br>
     <button type="submit" value="Submit">Submit</button> 
     <button type="reset" value="Reset">Clear</button>
