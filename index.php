aa8f004b465295281974c80a6d6b001078b11998
Minebruke <80499695+Minebruke@users.noreply.github.com>
2021-03-19 09:10:16 +0600

    Update index.php

--- index.php
+++ index.php
@@ -16,8 +16,6 @@
  <form action="action_page.php">
   <div class="container">
     <h1>Register</h1>
-    <p>Please fill in this form to create an account.</p>
-    <hr>
 
     <label for="email"><b>Email</b></label>
     <input type="text" placeholder="Enter Email" name="email" id="email" required>
@@ -25,12 +23,8 @@
     <label for="psw"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
 
-    <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
-    <button type="submit" class="registerbtn">Register</button>
-  </div>
-
-  <div class="container signin">
-    <p>Already have an account? <a href="#">Sign in</a>.</p>
+    <button type="submit" value="Submit">Submit</button> 
+    <button type="reset" value="Reset">Clear</button>
   </div>
 </form> 
  </body>
