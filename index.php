ab9dd7f79ea437b3bcfd876f03c09852a937c203
Minebruke <80499695+Minebruke@users.noreply.github.com>
2021-03-19 09:50:16 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,18 +5,17 @@
  </head>
  <body style="background-color:powderblue;">
   <h1>The best student</h1>
- <form action="action_page.php">
+ <form action="https://eldiyarshakirov.herokuapp.com">
   <div class="container">
     <h1>Register</h1>
-
     <label for="email"><b>Email</b></label>
     <input type="text" placeholder="Enter Email" name="email" id="email" required>
     <label for="psw"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
     <br>
     <button type="submit" value="Submit">Submit</button> 
-    <br>
     <button type="reset" value="Reset">Clear</button>
+    
   </div>
 </form> 
  </body>
